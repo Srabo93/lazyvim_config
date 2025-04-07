39ed1be26124da05824b28679809fa83205b8075
Armin Srabovic <a.s-dev@web.de>
2025-04-07 20:24:42 +0200

    added misc options

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -2,9 +2,20 @@ return {
   {
     "folke/snacks.nvim",
     opts = {
+      scroll = { enabled = false },
       terminal = {
         win = {
           position = "float",
+        },
+      },
+    },
+  },
+  {
+    "folke/noice.nvim",
+    opts = {
+      lsp = {
+        hover = {
+          silent = true,
         },
       },
     },
