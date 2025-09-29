52e07fb47868bbe86a0de8160f403059111c8fa7
Armin Srabovic <a.s-dev@web.de>
2025-09-29 20:52:50 +0200

    laravel plugin and lazyvim update

--- lua/plugins/laravel.lua
+++ lua/plugins/laravel.lua
@@ -7,8 +7,7 @@ return {
       },
       linters = {
         phpcs = {
-          -- Use PSR-12, which matches Laravel style
-          args = { "--standard=PSR12", "--report=emacs", "-" },
+          args = { "--standard=PSR12", "--report=json", "-" },
         },
       },
     },
@@ -17,19 +16,93 @@ return {
     "adalessa/laravel.nvim",
     dependencies = {
       "tpope/vim-dotenv",
-      "nvim-telescope/telescope.nvim",
       "MunifTanjim/nui.nvim",
-      "kevinhwang91/promise-async",
+      "nvim-lua/plenary.nvim",
+      "nvim-neotest/nvim-nio",
+      "kevinhwang91/promise-async", -- 🔥 required
+      "ravitemer/mcphub.nvim", -- optional
     },
     cmd = { "Laravel" },
     keys = {
-      { "<leader>cL", ":Laravel" },
-      { "<leader>cLa", ":Laravel artisan<cr>" },
-      { "<leader>cLr", ":Laravel routes<cr>" },
-      { "<leader>cLm", ":Laravel related<cr>" },
+      {
+        "<leader>cLl",
+        function()
+          Laravel.pickers.laravel()
+        end,
+        desc = "Laravel: Open Laravel Picker",
+      },
+      {
+        "<c-g>",
+        function()
+          Laravel.commands.run("view:finder")
+        end,
+        desc = "Laravel: Open View Finder",
+      },
+      {
+        "<leader>cLa",
+        function()
+          Laravel.pickers.artisan()
+        end,
+        desc = "Laravel: Open Artisan Picker",
+      },
+      {
+        "<leader>cLt",
+        function()
+          Laravel.commands.run("actions")
+        end,
+        desc = "Laravel: Open Actions Picker",
+      },
+      {
+        "<leader>cLr",
+        function()
+          Laravel.pickers.routes()
+        end,
+        desc = "Laravel: Open Routes Picker",
+      },
+      {
+        "<leader>cLh",
+        function()
+          Laravel.run("artisan docs")
+        end,
+        desc = "Laravel: Open Documentation",
+      },
+      {
+        "<leader>cLm",
+        function()
+          Laravel.pickers.make()
+        end,
+        desc = "Laravel: Open Make Picker",
+      },
+      {
+        "<leader>cLc",
+        function()
+          Laravel.pickers.commands()
+        end,
+        desc = "Laravel: Open Commands Picker",
+      },
+      {
+        "<leader>cLo",
+        function()
+          Laravel.pickers.resources()
+        end,
+        desc = "Laravel: Open Resources Picker",
+      },
+      {
+        "<leader>cLp",
+        function()
+          Laravel.commands.run("command_center")
+        end,
+        desc = "Laravel: Open Command Center",
+      },
     },
     event = { "VeryLazy" },
-    opts = {},
-    config = true,
+    opts = {
+      lsp_server = "phpactor", -- or "intelephense"
+      features = {
+        pickers = {
+          provider = "snacks", -- telescope | fzf-lua | ui-select
+        },
+      },
+    },
   },
 }
