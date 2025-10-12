aa5e5e87237397c6535a769509d879135ba481f8
Armin Srabovic <a.s-dev@web.de>
2025-10-12 15:13:24 +0200

    fix theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,10 +1,6 @@
 return {
   {
     "folke/tokyonight.nvim",
-    lazy = false, -- load immediately
-    priority = 1000, -- load before other start plugins
-    config = function()
-      vim.cmd.colorscheme("tokyonight")
-    end,
+    opts = { style = "night" },
   },
 }
